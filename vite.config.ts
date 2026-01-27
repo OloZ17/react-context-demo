ded6ffb7ffae660a90696a7d2c433bc3f050cc3e
Thomas Lamarche <glims@wanadoo.fr>
2026-01-27 17:08:55 +0100

    conflicts resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
@@ -22,4 +28,4 @@ export default defineConfig({
       ],
     },
   },
-})
+});
